965b71d95cd3a7d3023f348067027523bb1c91f5
angepnt021 <pintoangelica0210@gmail.com>
2019-06-13 13:40:17 +0200

    Agregar archivos de proyecto.

---
+++ Conectar Tablas/Data/ConectarTablasContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Conectar_Tablas.Models;
+
+    public class ConectarTablasContext : DbContext
+    {
+        public ConectarTablasContext (DbContextOptions<ConectarTablasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Conectar_Tablas.Models.Persona> Persona { get; set; }
+        public DbSet<Conectar_Tablas.Models.Dni> Dni { get; set; }
+    }
